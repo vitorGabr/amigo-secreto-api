cc146462e0533922b00a9b726c088ce337897353
vitorGabr <macstudios.info@gmail.com>
2024-11-22 19:30:14 -0300

    Refactor authentication link handling to streamline user sign-in process and improve redirect logic

--- src/http/routes/auth/authenticate-link.ts
+++ src/http/routes/auth/authenticate-link.ts
@@ -4,17 +4,17 @@ import Elysia, { t } from "elysia";
 
 export const authenticateLink = new Elysia().use(authentication).get(
 	"/auth/email/verify",
-	async ({ signUser, query, redirect: internalRedirect }) => {
+	async ({ signUser, query, redirect }) => {
 		const makeAuthenticateFromLink = makeAuthenticateLinkUseCase();
 		const reponse = await makeAuthenticateFromLink.execute({
 			code: query.code,
 		});
-		const token = await signUser({
+		await signUser({
 			sub: reponse.userId,
 		});
 
 		const redirectUrl = process.env.AUTH_REDIRECT_URL;
-		return redirectUrl ? internalRedirect(redirectUrl) : token;
+		return redirectUrl && redirect(redirectUrl, 302);
 	},
 	{
 		query: t.Object({
